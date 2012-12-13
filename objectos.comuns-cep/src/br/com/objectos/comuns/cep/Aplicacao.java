7f6a60b35270fb6823d7f360710561960984a40d
Gustavo Melo <gustavo.melo@objectos.com.br>
2012-12-13 14:47:23 -0200

    Update objectos.comuns-cep/src/br/com/objectos/comuns/cep/Aplicacao.java

--- objectos.comuns-cep/src/br/com/objectos/comuns/cep/Aplicacao.java
+++ objectos.comuns-cep/src/br/com/objectos/comuns/cep/Aplicacao.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 public class Aplicacao {
 	static String cep = "";
 
-	public static void main(String[] args) { //@marcioendo teste
+	public static void main(String[] args) { 
 		Scanner entrada = new Scanner(System.in);
 		String r = "s";
 		while (r.equals("s")) {
